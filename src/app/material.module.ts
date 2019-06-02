7702d46b8d9296210440c91c274875df22a249c7
Christopher <premnath.christopher@siemens.com>
2019-06-02 12:31:29 +0530

    Sign up form is implemented

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatButtonModule, MatCheckboxModule, MatIconModule,MatFormFieldModule,
+    MatInputModule,MatDatepickerModule, MatNativeDateModule
+} from '@angular/material';
 
 @NgModule({
-    imports: [MatButtonModule, MatCheckboxModule],
-    exports: [MatButtonModule, MatCheckboxModule]
+    imports: [MatButtonModule, MatCheckboxModule,MatIconModule,MatFormFieldModule,
+        MatInputModule,MatDatepickerModule,MatNativeDateModule],
+    exports: [MatButtonModule, MatCheckboxModule,MatIconModule,MatFormFieldModule,
+        MatInputModule,MatDatepickerModule,MatNativeDateModule]
 })
 export class MaterialModule {}
